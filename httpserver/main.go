06661b751e718870d74f59b3b0547e081b4e0177
tjudream <laomeng_boy@163.com>
2022-02-24 21:19:31 +0800

    add graceful shutdown

--- httpserver/main.go
+++ httpserver/main.go
@@ -1,29 +1,38 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"io"
 	"log"
 	"net"
 	"net/http"
 	"os"
-	"strings"
+	"os/signal"
 	"strconv"
+	"strings"
+	"syscall"
+	"time"
 )
 
 func main() {
 	port := 8080
-	http.HandleFunc("/", HttpHandler)
-	http.HandleFunc("/healthz", HealthZ)
-
-	portstr := ":" +  strconv.Itoa(port)
+	portstr := ":" + strconv.Itoa(port)
 	log.Printf("httpserver listend port: %+v", port)
-	err := http.ListenAndServe(portstr, nil)
-	if err != nil {
-		log.Fatal(err)
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", HttpHandler)
+	mux.HandleFunc("/healthz", HealthZ)
+	mux.HandleFunc("/sleep", SleepTest)
+	server := &http.Server{
+		Addr:         portstr,
+		Handler:      mux,
 	}
+	go server.ListenAndServe()
+	listenSignal(context.Background(), server)
 }
 
-func HealthZ(w http.ResponseWriter, r *http.Request)  {
+func HealthZ(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(200)
 }
 func HttpHandler(w http.ResponseWriter, r *http.Request) {
@@ -32,9 +41,9 @@ func HttpHandler(w http.ResponseWriter, r *http.Request) {
 	log.Printf("reqLog: clientIP : %+v, httpcode : %+v", ip, httpcode)
 	headers := r.Header
 	log.Printf("req headers : %+v", headers)
-	for k,v := range headers {
-		for _,val := range v {
-			w.Header().Set(k,val)
+	for k, v := range headers {
+		for _, val := range v {
+			w.Header().Set(k, val)
 		}
 	}
 	version := os.Getenv("VERSION")
@@ -59,4 +68,28 @@ func ClientIP(r *http.Request) string {
 		return ip
 	}
 	return ""
+}
+
+func SleepTest(w http.ResponseWriter, r *http.Request) {
+	values := r.URL.Query()
+	sleeptimeStr := values.Get("sleep")
+	sleeptime, err := strconv.Atoi(sleeptimeStr)
+	if err != nil {
+		sleeptime = 1
+	}
+	time.Sleep(time.Duration(sleeptime) * time.Second)
+	fmt.Fprintln(w, "Hello world, sleep " + strconv.Itoa(sleeptime) + "s")
+	log.Printf( "Hello world, sleep %+vs", sleeptime)
+}
+
+func listenSignal(ctx context.Context, httpSrv *http.Server) {
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
+
+	select {
+	case <-sigs:
+		fmt.Println("notify sigs")
+		httpSrv.Shutdown(ctx)
+		fmt.Println("http shutdown gracefully")
+	}
 }
