1a33565539e563a4bcc9521a5bdd56c81db90021
tjudream <laomeng_boy@163.com>
2022-03-12 11:27:38 +0800

    优雅终止

--- httpserver/main.go
+++ httpserver/main.go
@@ -3,8 +3,10 @@ package main
 import (
 	"context"
 	"fmt"
+	"github.com/fsnotify/fsnotify"
+	log "github.com/sirupsen/logrus"
+	"github.com/spf13/viper"
 	"io"
-	"log"
 	"net"
 	"net/http"
 	"os"
@@ -14,11 +16,34 @@ import (
 	"syscall"
 	"time"
 )
+var logLevel string
+var viperInstance *viper.Viper
 
 func main() {
+	httpserverConf := os.Getenv("HTTPSERVER_CONF")
+	log.Info("configFile from env is " + httpserverConf)
+	if httpserverConf == "" {
+		httpserverConf = "/etc/httpserver/httpserver.properties"
+	}
+	log.Info("confFile is " + httpserverConf)
+	viperInstance = viper.New()	// viper实例
+	viperInstance.SetConfigFile(httpserverConf) // 指定配置文件路径
+
+	err := viperInstance.ReadInConfig()
+	if err != nil { // 处理读取配置文件的错误
+		panic(fmt.Errorf("Fatal error config file: %s \n", err))
+	}
+	viperInstance.WatchConfig()
+	viperInstance.OnConfigChange(func(e fsnotify.Event) {
+		fmt.Println("Detect config change: %s \n", e.String())
+		log.Warn("Config file updated.")
+		viperLoadConf(viperInstance)  // 加载配置的方法
+	})
+	viperLoadConf(viperInstance)
+
 	port := 8080
 	portstr := ":" + strconv.Itoa(port)
-	log.Printf("httpserver listend port: %+v", port)
+	log.Info("httpserver listend port: ", port)
 
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", HttpHandler)
@@ -32,15 +57,45 @@ func main() {
 	listenSignal(context.Background(), server)
 }
 
+func dynamicConfig() {
+	viperInstance.WatchConfig()
+	viperInstance.OnConfigChange(func(e fsnotify.Event) {
+		fmt.Println("Detect config change: %s \n", e.String())
+		log.Warn("Config file updated.")
+		viperLoadConf(viperInstance)  // 加载配置的方法
+	})
+}
+
+func viperLoadConf(viperInstance *viper.Viper) {
+	logLevel = viperInstance.GetString("log_level")
+	level, err := log.ParseLevel(logLevel)
+	if err != nil {
+		level = log.GetLevel()
+	}
+	log.SetLevel(level)
+	myconf := viperInstance.GetString("my_conf")
+	log.Trace(myconf + " in viperLoadConf")
+	log.Debug(myconf + " in viperLoadConf")
+	log.Info(myconf + " in viperLoadConf")
+	log.Warn(myconf + " in viperLoadConf")
+	log.Error(myconf + " in viperLoadConf")
+}
+
 func HealthZ(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(200)
 }
 func HttpHandler(w http.ResponseWriter, r *http.Request) {
+	myconf := viperInstance.GetString("my_conf")
+	log.Trace(myconf)
+	log.Debug(myconf)
+	log.Info(myconf)
+	log.Warn(myconf)
+	log.Error(myconf)
 	ip := ClientIP(r)
 	httpcode := 200
-	log.Printf("reqLog: clientIP : %+v, httpcode : %+v", ip, httpcode)
+	log.Info("reqLog: clientIP : " + ip + " httpcode " + strconv.Itoa(httpcode))
 	headers := r.Header
-	log.Printf("req headers : %+v", headers)
+	log.Info("req headers : %+v", headers)
 	for k, v := range headers {
 		for _, val := range v {
 			w.Header().Set(k, val)
@@ -82,6 +137,7 @@ func SleepTest(w http.ResponseWriter, r *http.Request) {
 	log.Printf( "Hello world, sleep %+vs", sleeptime)
 }
 
+// 优雅终止
 func listenSignal(ctx context.Context, httpSrv *http.Server) {
 	sigs := make(chan os.Signal, 1)
 	signal.Notify(sigs, syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
